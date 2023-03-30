8fe08530982e41745bf72d363c4a41c57a512964
Anass Barriz <anass1.barriz@epita.fr>
2023-03-30 17:53:20 +0200

    version2

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,5 +4,21 @@ public class Launcher {
     public static void main(String[] args) {
         System.out.println("Bienvenue dans Launcher programme !");
         Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+
+        while(!userInput.equals("quit")) {
+            System.out.println("Veuillez entrer une commande (ou 'quit' pour quitter):");
+            userInput = scanner.nextLine();
+            
+            switch (userInput) {
+                // Add cases for valid commands here
+                default:
+                    System.out.println("Unknown command");
+                    break;
+            }
+        }
+        
+        System.out.println("Au revoir !");
     }
 }
+
