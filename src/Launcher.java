138642ed58f37165c20e663c34d27544f7f00cef
Anass Barriz <anass1.barriz@epita.fr>
2023-03-30 17:26:52 +0200

    version1

---
+++ src/Launcher.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Bienvenue dans Launcher programme !");
+        Scanner scanner = new Scanner(System.in);
+    }
+}
